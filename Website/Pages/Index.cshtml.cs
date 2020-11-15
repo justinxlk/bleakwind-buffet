88327791ed4747b09dc574cd4fd39f9484445c1f
justinxlk <60164559+justinxlk@users.noreply.github.com>
2020-11-15 14:38:02 -0600

    Responsive Design is all that I have left to do for the functionality of Web Milestone 3.

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Sides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -35,18 +37,38 @@ namespace Website.Pages
             if (SearchTerms != null)
             {
                 SearchTermArray = SearchTerms.Split(' ');
-                foreach (string searchTerm in SearchTermArray)
-                {
+                //foreach (string searchTerm in SearchTermArray)
+                //{
                     //Items = Items.Where(item => item.StringName != null && item.StringName.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase));
                     IEnumerable<IOrderItem> enumerable = Items.Where(item => item.StringName != null && (SearchTermArray.Any(item.StringName.Contains) || SearchTermArray.Any(item.Description.Contains)));
                     Items = enumerable;
-                }
+                //}
             }
-
             //Items = FilterLogic.Search(SearchTerms);
-            Items = FilterLogic.FilterByItemType(Items, itemCoursesLabels);
-            Items = FilterLogic.FilterByPrice(Items, PriceMin, PriceMax);
-            Items = FilterLogic.FilterByCalories(Items, CalMin, CalMax);
+            if (itemCoursesLabels != null && itemCoursesLabels.Length > 0)
+            {
+                IEnumerable<IOrderItem> enumerable = Items.Where(item => (item is Drink && itemCoursesLabels.Contains("Drink")) || (item is Entree && itemCoursesLabels.Contains("Entree")) || (item is Side && itemCoursesLabels.Contains("Side")));
+                Items = enumerable;
+            }
+            //Items = FilterLogic.FilterByItemType(Items, itemCoursesLabels);
+            if (PriceMax != null)
+            {
+                Items = Items.Where(item => item.Price != null && item.Price <= PriceMax);
+            }
+            if (PriceMin != null)
+            {
+                Items = Items.Where(item => item.Price != null && item.Price >= PriceMin);
+            }
+            //Items = FilterLogic.FilterByPrice(Items, PriceMin, PriceMax);
+            if (CalMax != null)
+            {
+                Items = Items.Where(item => item.Calories != null && item.Calories <= CalMax);
+            }
+            if (CalMin != null)
+            {
+                Items = Items.Where(item => item.Calories != null && item.Calories >= CalMin);
+            }
+            //Items = FilterLogic.FilterByCalories(Items, CalMin, CalMax);
         }
 
         public string SearchTerms { get; set; }
